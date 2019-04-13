9609c40a2d87d055ef617d52d4d95a0ab010506e
RicardoLinares <reli@live.com.ar>
2019-04-13 01:42:37 -0300

    4/13

--- Calculadora/Entidades/Numero.cs
+++ Calculadora/Entidades/Numero.cs
@@ -25,77 +25,122 @@ namespace Entidades
             Double.TryParse(strNumero, out resultado);
             return resultado;
         }
-        public static string BinarioDecimal(string binario)
+        public static string BinarioDecimal(string strNumero)
         {
             string resultado = "Valor Invalido";
-            string parteDecimal = "";
-            string parteFraction = "";
-            bool valorValido = true;
-            foreach (char c in binario)
+            if (Double.TryParse(strNumero, out double numero))
             {
-                int numero;
-                if(c == '1' && c == '0')
+
+                double acumResultado = 0;
+                string parteEnteraBinaria = "";
+                bool esDecimal = false;
+                string parteEntera = "";
+                string parteDecimal = "";
+                bool valorValido = true;
+                foreach (char caracterNumerico in strNumero)
                 {
-                    parteDecimal += c;
+                    if (Int32.TryParse(caracterNumerico.ToString(), out int x))
+                    {
+                        if (esDecimal)
+                        {
+                            parteDecimal += caracterNumerico;
+                        }
+                        else
+                        {
+                            parteEntera += caracterNumerico;
+                        }
+                    }
+                    else if (caracterNumerico == '.' && esDecimal == false)
+                    {
+                        esDecimal = true;
+                    }
+                    else
+                    {
+                        valorValido = false;
+                    }
                 }
-                else if(Byte.TryParse(c.ToString(), numero))
+                if (valorValido)
                 {
-                    valorValido = false;
-                    break;
+                    for (int i = parteEntera.Length; i > 0 ; i--)
+                    {
+                        acumResultado += Convert.ToInt32(parteEntera.ElementAt(parteEntera.Length - i).ToString()) * Math.Pow(2, i - 1);
+                    }
+                    if (esDecimal)
+                    {
+                        for (int i = 0; i < parteDecimal.Length; i++)
+                        {
+                            acumResultado += Convert.ToInt32(parteDecimal.ElementAt(i).ToString()) * Math.Pow(2, -(i + 1));
+                        }
+                    }
+                    resultado = acumResultado.ToString();
                 }
-            }
-            if(valorValido)
-            {
-                resultado = Convert.ToInt32(parteDecimal, 2).ToString();
+
             }
             return resultado;
         }
 
         public static string DecimalBinario(double numero)
         {
-            string binario;
-            try
-            {
-                int conversion = Convert.ToInt32(numero);
-                binario = Convert.ToString(conversion, 2);
-                double parteDecimal = numero - conversion;
-                if(parteDecimal > 0)
-                {
-                    binario += ".";
-                }
-                for (int i = 0; i < 20 && parteDecimal != 0d; i++)
-                {
-                    parteDecimal = parteDecimal * 2;
-                    if (parteDecimal >= 1d)
-                    {
-                        parteDecimal -= 1.0;
-                        binario += "1";
-                    }
-                    else
-                    {
-                        binario += "0";
-                    }
-                }
-            }
-            catch(OverflowException)
-            {
-                binario = "Valor Invalido";
-            }
-            finally
-            {
-
-            }
-
-            return binario;
+            return DecimalBinario(numero);
         }
 
         public static string DecimalBinario(string strNumero)
         {
             string binario = "Valor Invalido";
             double numero;
-            if(Double.TryParse(strNumero, out numero))
+            if (Double.TryParse(strNumero, out numero))
             {
-                binario = DecimalBinario(numero);
+                bool esDecimal = false;
+                string parteEnteraStr = "";
+                double parteEntera;
+                double parteDecimal = 0;
+                bool valorValido = true;
+                foreach (char caracterNumerico in strNumero)
+                {
+                    if (Int32.TryParse(caracterNumerico.ToString(), out int x))
+                    {
+                        if (!esDecimal)
+                        {
+                            parteEnteraStr += caracterNumerico;
+                        }
+                    }
+                    else if (caracterNumerico == '.' && esDecimal == false)
+                    {
+                        esDecimal = true;
+                        parteDecimal = numero - ((long)numero);
+                    }
+                    else
+                    {
+                        valorValido = false;
+                    }
+                }
+                if (valorValido)
+                {
+                    parteEntera = Convert.ToInt32(parteEnteraStr);
+                    binario = Convert.ToString(Convert.ToInt32(parteEntera), 2);
+                    if (esDecimal)
+                    {
+                        binario += ".";
+                        for (int i = 0; i < 40; i++)
+                        {
+                            parteDecimal = parteDecimal * 2;
+                            if(parteDecimal >= 1)
+                            {
+                                binario += "1";
+                                parteDecimal -= 1d;
+                                if(parteDecimal == 0d)
+                                {
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                binario += "0";
+                            }
+                        }
+                    }
+                }
+
             }
             return binario;
         }
